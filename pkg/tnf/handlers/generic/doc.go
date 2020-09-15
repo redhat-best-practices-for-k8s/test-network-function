04a8f7473daf704e2d54250026bb43e38ffbdc0a
Ryan Goulding <rgouldin@redhat.com>
2020-09-15 17:27:42 -0400

    Define declarative tnf.Test(s) handler layout and match definition
    Defines the "match" struct.  Since match has no actual implementation dependent
    code, it requires no testing.
    
    Introduces the generic module, and documents appropriately.  Future commits
    will build on top of this as a part of CTONET-525.
    
    Signed-off-by: Ryan Goulding <rgouldin@redhat.com>

---
+++ pkg/tnf/handlers/generic/doc.go
@@ -0,0 +1,5 @@
+/*
+Package generic provides a simplified interface for creating a tnf.Test and corresponding reel.Handler from generic JSON
+input.  This exposes the ability for test writers to create tests using only JSON.
+*/
+package generic
