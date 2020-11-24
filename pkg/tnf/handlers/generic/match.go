1e2cf3d5d1534019d606e8c9e6d79d5f3a7129a6
Ryan Goulding <rgouldin@redhat.com>
2020-11-24 10:41:43 -0500

    Define declarative tnf.Test(s) handler layout and match definition (#33)
    * Define declarative tnf.Test(s) handler layout and match definition
    
    Defines the "match" struct.  Since match has no actual implementation dependent
    code, it requires no testing.
    
    Introduces the generic module, and documents appropriately.  Future commits
    will build on top of this as a part of CTONET-525.
    
    Signed-off-by: Ryan Goulding <rgouldin@redhat.com>
    
    * fixup! Define declarative tnf.Test(s) handler layout and match definition

---
+++ pkg/tnf/handlers/generic/match.go
@@ -0,0 +1,15 @@
+package generic
+
+// Match follows the Container design pattern, and is used to store the arguments to a reel.Handler's ReelMatch
+// function in a single data transfer object.
+type Match struct {
+
+	// Pattern is the pattern causing a match in reel.Handler ReelMatch.
+	Pattern string `json:"pattern,omitempty" yaml:"pattern,omitempty"`
+
+	// Before contains all output preceding Match.
+	Before string `json:"before,omitempty" yaml:"before,omitempty"`
+
+	// Match is the matched string.
+	Match string `json:"match,omitempty" yaml:"match,omitempty"`
+}
