b2a7339d1baf28cfc314fa7ffc0717d8198ed07d
Aneesh Puttur <aneeshputtur@gmail.com>
2020-12-11 09:33:23 -0500

    fixup! Append new claim files

--- cmd/claim/main.go
+++ cmd/claim/main.go
@@ -1,3 +1,19 @@
+// Copyright (C) 2020 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 package main
 
 import (
@@ -40,8 +56,7 @@ func main() {
 	// os.Arg[0] is the main command
 	// os.Arg[1] will be the subcommand
 	if len(os.Args) < argsLen {
-		fmt.Println("claim-add subcommand is required")
-		os.Exit(1)
+		log.Fatalf("claim-add subcommand is required")
 	}
 
 	// Switch on the subcommand
@@ -111,7 +126,7 @@ func claimUpdate() {
 	if fileUpdated {
 		log.Printf("Claim file `%s` updated\n", *claimFileTextPtr)
 	} else {
-		log.Printf("No changes were applied to  `%s`\n", *claimFileTextPtr)
+		log.Printf("No changes were applied to `%s`\n", *claimFileTextPtr)
 	}
 }
 
