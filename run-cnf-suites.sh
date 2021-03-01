6087a5d845a345f147870ba11713db8fea507cd7
Charlie Wheeler-Robinson <cwheeler@redhat.com>
2021-03-01 18:50:16 +0000

    Add flag to control claim output location
    Signed-off-by: Charlie Wheeler-Robinson <cwheeler@redhat.com>

--- run-cnf-suites.sh
+++ run-cnf-suites.sh
@@ -1,38 +1,57 @@
 #!/usr/bin/env bash
-COMMON_GINKGO_ARGS="-ginkgo.v -junit . -report ."
-FOCUS=""
+
+# defaults
+OUTPUT_LOC="."
 
 usage() {
+	echo "$0 [-o OUTPUT_LOC] SUITE [... SUITE]"
 	echo "Call the script and list the test suites to run"
 	echo "  e.g."
 	echo "    $0 generic container"
 	echo "  will run the generic and container suites"
 	echo ""
 	echo "Allowed suites are listed in the README."
+}
+
+usage_error() {
+	usage
 	exit 1
 }
+
+# Parge args beginning with "-"
+while [[ $1 == -* ]]; do
+	case "$1" in
+		-h|--help|-\?) usage; exit 0;;
+		-o) if (($# > 1)); then
+				OUTPUT_LOC=$2; shift 2
+			else
+				echo "-o requires an argument" 1>&2
+				exit 1
+			fi ;;
+		--) shift; break;;
+		-*) echo "invalid option: $1" 1>&2; usage_error;;
+	esac
+done
+
+GINKGO_ARGS="-ginkgo.v -junit $OUTPUT_LOC -report $OUTPUT_LOC -claimloc $OUTPUT_LOC"
+FOCUS=""
 
 for var in "$@"
 do
 	case "$var" in
-	  diagnostic) FOCUS="diagnostic|$FOCUS"
-	    ;;
-		generic) FOCUS="generic|$FOCUS"
-			;;
-		multus) FOCUS="multus|$FOCUS"
-			;;
-		operator) FOCUS="operator|$FOCUS"
-			;;
-		container) FOCUS="container|$FOCUS"
-			;;
-		*) usage
-			;;
+		diagnostic) FOCUS="diagnostic|$FOCUS";;
+		generic) FOCUS="generic|$FOCUS";;
+		multus) FOCUS="multus|$FOCUS";;
+		operator) FOCUS="operator|$FOCUS";;
+		container) FOCUS="container|$FOCUS";;
+		*) usage_error;;
 	esac
 done
 
-[ -z "$FOCUS" ] && usage
+# If no focus is set then display usage and quit with a non-zero exit code.
+[ -z "$FOCUS" ] && usage_error
 
 FOCUS=${FOCUS%?}  # strip the trailing "|" from the concatenation
 
-echo "running with focus '$FOCUS'"
-cd ./test-network-function && ./test-network-function.test -ginkgo.focus="$FOCUS" ${COMMON_GINKGO_ARGS}
+echo "Running with focus '$FOCUS'. Report will be output to '$OUTPUT_LOC'"
+cd ./test-network-function && ./test-network-function.test -ginkgo.focus="$FOCUS" ${GINKGO_ARGS}
