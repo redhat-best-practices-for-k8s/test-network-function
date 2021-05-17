a1477f17053cba1cf9e497bbde239c812d964bb6
Charlie Wheeler-Robinson <cwheeler@redhat.com>
2021-05-17 18:36:37 +0100

    add automatic config discovery for container spec
    This adds functionality to allow the `container` spec to
    automatically discover configuration from the cluster
    
    Signed-off-by: Charlie Wheeler-Robinson <cwheeler@redhat.com>

---
+++ pkg/config/autodiscover/autodiscover_cnfs.go
@@ -0,0 +1,78 @@
+// Copyright (C) 2021 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package autodiscover
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/test-network-function/test-network-function/pkg/config/configsections"
+	"github.com/test-network-function/test-network-function/pkg/tnf/testcases"
+)
+
+const (
+	cnfLabelName       = "container"
+	configuredTestFile = "testconfigure.yml"
+)
+
+var (
+	cnfTestsAnnotationName = buildAnnotationName("container_tests")
+)
+
+// BuildCNFsConfig builds a `[]configsections.Cnf` from the current state of the cluster,
+// using labels and annotations to populate the data.
+func BuildCNFsConfig() (cnfs []configsections.Cnf) {
+	pods, err := GetPodsByLabel(cnfLabelName, AnyLabelValue)
+	if err != nil {
+		log.Warnf("an error (%s) occurred when getting the FS Diff Master Container. Attempting to continue", err)
+		return cnfs
+	}
+	for i := range pods.Items {
+		cnfs = append(cnfs, BuildCnfFromPodResource(&pods.Items[i]))
+	}
+	return cnfs
+}
+
+// BuildCnfFromPodResource builds a single `configsections.Cnf` from a PodResource
+func BuildCnfFromPodResource(pr *PodResource) (cnf configsections.Cnf) {
+	var err error
+	cnf.Namespace = pr.Metadata.Namespace
+	cnf.Name = pr.Metadata.Name
+
+	var tests []string
+	err = pr.GetAnnotationValue(cnfTestsAnnotationName, &tests)
+	if err != nil {
+		log.Warnf("unable to extract tests from annotation on '%s/%s' (error: %s). Attempting to fallback to all tests", cnf.Namespace, cnf.Name, err)
+		cnf.Tests = getConfiguredCNFTests()
+	} else {
+		cnf.Tests = tests
+	}
+	return
+}
+
+// getConfiguredCNFTests loads the `configuredTestFile` used by the `operator` and `container` specs, and extracts
+// the names of test groups from it.
+func getConfiguredCNFTests() (cnfTests []string) {
+	configuredTests, err := testcases.LoadConfiguredTestFile(configuredTestFile)
+	if err != nil {
+		log.Errorf("failed to load %s, continuing with no tests", configuredTestFile)
+		return []string{}
+	}
+	for _, configuredTest := range configuredTests.CnfTest {
+		cnfTests = append(cnfTests, configuredTest.Name)
+	}
+	log.WithField("cnfTests", cnfTests).Infof("got all tests from %s.", configuredTestFile)
+	return cnfTests
+}
---
+++ pkg/config/autodiscover/container_test.go
@@ -0,0 +1,42 @@
+// Copyright (C) 2021 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package autodiscover_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/test-network-function/test-network-function/pkg/config/autodiscover"
+)
+
+func TestBuildCnfFromPodResource(t *testing.T) {
+	orchestratorPodResource := loadPodResource(testOrchestratorFilePath)
+	orchestratorPod := autodiscover.BuildCnfFromPodResource(&orchestratorPodResource)
+
+	subjectPodResource := loadPodResource(testSubjectFilePath)
+	subjectPod := autodiscover.BuildCnfFromPodResource(&subjectPodResource)
+
+	assert.Equal(t, "tnf", orchestratorPod.Namespace)
+	assert.Equal(t, "I'mAPodName", orchestratorPod.Name)
+	assert.NotEqual(t, "I'mAContainer", orchestratorPod.Name)
+	// no tests set on pod and the config file will not be loaded from the unit test context: no tests should be set.
+	assert.Equal(t, []string{}, orchestratorPod.Tests)
+
+	assert.Equal(t, "tnf", subjectPod.Namespace)
+	assert.Equal(t, "test", subjectPod.Name)
+	assert.Equal(t, []string{"OneTestName", "AnotherTestName"}, subjectPod.Tests)
+}
