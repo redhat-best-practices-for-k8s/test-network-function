674ae1fb3db4c1a20cda0a19d27e7fad9ca1799a
Shimrit peretz <speretz@redhat.com>
2021-07-20 19:30:06 +0300

    updated handler name with cobra

--- cmd/tnf_cobra/main.go
+++ cmd/tnf_cobra/main.go
@@ -10,52 +10,60 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type handler struct {
+type myHandler struct {
 	X string
 }
 
 var (
-	tnf                string
-	generate           string
-	myhandler          string
 	pathrelativetoroot string
 	handlerDirectory   string
 	handlername        string
-	rootCmd            = &cobra.Command{
+
+	rootCmd = &cobra.Command{
+		Use:   "rootCmd",
+		Short: "test network function command line tools",
+	}
+
+	generate = &cobra.Command{
+		Use:   "generate",
+		Short: "generator tool for various tnf artifacts, such as handler code, catalog etc",
+	}
+
+	handler = &cobra.Command{
 		Use:   "handler",
 		Short: "adding new handler",
-		Run: func(cmd *cobra.Command, args []string) {
-			tnf = args[0]
-			generate = args[1]
-			myhandler = args[2]
-			handlername = args[len(args)-1]
-			pathrelativetoroot = path.Join("..", "..")
-			handlerDirectory = path.Join(pathrelativetoroot, "pkg", "tnf", "handlers")
-			newHandlerDirectory := path.Join(handlerDirectory, handlername)
-
-			os.Mkdir(newHandlerDirectory, 0755)
-
-			// create 3 files by template
-
-			myhandler := handler{X: handlername}
-
-			pathfile := path.Join(handlerDirectory, "handler_template", "doc.tmpl")
-			namefile := "doc.tmpl"
-			createfile(pathfile, namefile, myhandler, newHandlerDirectory)
-
-			pathfile = path.Join(handlerDirectory, "handler_template", "template_test.tmpl")
-			namefile = "" + handlername + "_test.tmpl"
-			createfile(pathfile, namefile, myhandler, newHandlerDirectory)
-
-			pathfile = path.Join(handlerDirectory, "handler_template", "template.tmpl")
-			namefile = "" + handlername
-			createfile(pathfile, namefile, myhandler, newHandlerDirectory)
-
-		},
+		Run:   addingHandler,
 	}
 )
 
-func createfile(pathfile string, namefile string, myhandler handler, newHandlerDirectory string) {
+func addingHandler(cmd *cobra.Command, args []string) {
+
+	handlername = args[0]
+	pathrelativetoroot = path.Join("..", "..")
+	handlerDirectory = path.Join(pathrelativetoroot, "pkg", "tnf", "handlers")
+	newHandlerDirectory := path.Join(handlerDirectory, handlername)
+
+	os.Mkdir(newHandlerDirectory, 0755)
+
+	// create 3 files by template
+
+	myhandler := myHandler{X: handlername}
+
+	pathfile := path.Join(handlerDirectory, "handler_template", "doc.tmpl")
+	namefile := "doc.tmpl"
+	createfile(pathfile, namefile, myhandler, newHandlerDirectory)
+
+	pathfile = path.Join(handlerDirectory, "handler_template", "template_test.tmpl")
+	namefile = "" + handlername + "_test.tmpl"
+	createfile(pathfile, namefile, myhandler, newHandlerDirectory)
+
+	pathfile = path.Join(handlerDirectory, "handler_template", "template.tmpl")
+	namefile = "" + handlername
+	createfile(pathfile, namefile, myhandler, newHandlerDirectory)
+}
+
+func createfile(pathfile string, namefile string, myhandler myHandler, newHandlerDirectory string) {
+
 	ftpl, err := template.ParseFiles(pathfile)
 	if err != nil {
 		log.Fatalln(err)
@@ -67,6 +75,7 @@ func createfile(pathfile string, namefile string, myhandler handler, newHandlerD
 	if err != nil {
 		panic(err)
 	}
+
 	defer f.Close()
 	w := bufio.NewWriter(f)
 
@@ -74,11 +83,13 @@ func createfile(pathfile string, namefile string, myhandler handler, newHandlerD
 	if err != nil {
 		panic(err)
 	}
+
 	w.Flush()
 }
 
 func main() {
-	rootCmd.SetArgs(os.Args[1:])
+	rootCmd.AddCommand(generate)
+	generate.AddCommand(handler)
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
