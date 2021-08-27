aaea790eebfcf46479df41d3ebf4679ed6707754
Salaheddine Hamadi <shamadi@redhat.com>
2021-08-27 17:20:31 -0400

    Shamadi release 30 (#327)
    * remove unnecessary folder
    
    * release v3.0.0

--- fs-diff-test-utils/diff-fs.sh
+++
@@ -1,7 +0,0 @@
-result=`podman diff $1 | cut -d " " -f 2 | grep -E "(^/var/lib/rpm)|(^/var/lib/dpkg)|(^/bin)|(^/sbin)|(^/lib)|(^/lib64)|(^/usr/bin)|(^/usr/sbin)|(^/usr/lib)|(^/usr/lib64)"`
-if [ -z "${result}" ]
-then
-echo empty
-else
-echo $result
-fi
