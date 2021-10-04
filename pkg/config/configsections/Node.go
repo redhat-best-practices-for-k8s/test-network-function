580c53d82db84058427cef320b1605cb9a5e7624
aabughosh <aabugosh@redhat.com>
2021-10-04 16:15:25 +0300

    CNFCERT-92:fix the liner

--- pkg/config/configsections/Node.go
+++ pkg/config/configsections/Node.go
@@ -19,13 +19,13 @@ package configsections
 const worker = "worker"
 const master = "master"
 
-// Nodes defines in the cluster. with name of the node and the type of this node master/worker,,,,.
+//Node defines in the cluster. with name of the node and the type of this node master/worker,,,,.
 type Node struct {
 	Name string
 	Type []string
 }
 
-// Function that return if the node is master
+//IsMaster Function that return if the node is master
 func (node Node) IsMaster() bool {
 	for _, t := range node.Type {
 		if t == master {
@@ -35,7 +35,7 @@ func (node Node) IsMaster() bool {
 	return false
 }
 
-// Function that return if the node is worker
+//IsWorker Function that return if the node is worker
 func (node Node) IsWorker() bool {
 	for _, t := range node.Type {
 		if t == worker {
