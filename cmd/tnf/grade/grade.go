dceaa361ff3770f827155e67698308b1e232ea6f
aabughosh <aabugosh@redhat.com>
2021-10-28 15:29:02 +0300

    Consolidate all cmds into one tnf cmd

---
+++ cmd/tnf/grade/grade.go
@@ -0,0 +1,53 @@
+package grade
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/test-network-function/test-network-function/pkg/gradetool"
+)
+
+var (
+	results    string
+	policy     string
+	OutputPath string
+
+	grade = &cobra.Command{
+		Use:   "gradetool",
+		Short: "gradetool",
+		RunE:  runGradetool,
+	}
+)
+
+func runGradetool(cmd *cobra.Command, args []string) error {
+	resultsPath := results
+	policyPath := policy
+	outputPath := OutputPath
+
+	err := gradetool.GenerateGrade(resultsPath, policyPath, outputPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return nil
+}
+func Execute() *cobra.Command {
+	grade.Flags().StringVarP(
+		&results, "results", "r", "",
+		"Path to the input test results file",
+	)
+
+	grade.Flags().StringVarP(
+		&policy, "policy", "p", "",
+		"Path to the input policy file",
+	)
+	grade.Flags().StringVarP(
+		&OutputPath, "OutputPath", "o", "",
+		"Path to the output file",
+	)
+	grade.MarkFlagRequired("results")
+	grade.MarkFlagRequired("policy")
+	grade.MarkFlagRequired("OutputPath")
+	return grade
+}
