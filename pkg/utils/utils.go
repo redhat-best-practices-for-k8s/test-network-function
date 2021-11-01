91009fef0e08085835f2cd1ce14f4a2586748dfd
edcdavid <86730676+edcdavid@users.noreply.github.com>
2021-11-01 18:15:57 -0500

    Checks if the claim/junit directory exists (#426)
    Co-authored-by: Salaheddine Hamadi <shamadi@redhat.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,13 @@
 package utils
 
-import "strings"
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
 
 // ArgListToMap takes a list of strings of the form "key=value" and translate it into a map
 // of the form {key: value}
@@ -26,4 +33,15 @@ func FilterArray(vs []string, f func(string) bool) []string {
 		}
 	}
 	return vsf
+}
+
+func CheckFileExists(path, name string) {
+	fullPath, _ := filepath.Abs(path)
+	if _, err := os.Stat(path); err == nil {
+		log.Infof("Path to %s file found and valid: %s ", name, fullPath)
+	} else if errors.Is(err, os.ErrNotExist) {
+		log.Fatalf("Path to %s file not found: %s , Exiting", name, fullPath)
+	} else {
+		log.Infof("Path to %s file not valid: %s , err=%s, exiting", name, fullPath, err)
+	}
 }
