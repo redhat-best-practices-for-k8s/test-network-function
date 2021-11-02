10d8490aa5f9bb6813f165e6898e6804588b79d1
aabughosh <aabugosh@redhat.com>
2021-11-02 11:46:20 +0200

    Consolidate all cmds into one tnf cmd

---
+++ cmd/tnf/jsontest/jsontest.go
@@ -0,0 +1,354 @@
+// Copyright (C) 2020-2021 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package jsontest
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+
+	expect "github.com/google/goexpect"
+	"github.com/google/goterm/term"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/test-network-function/test-network-function/pkg/tnf"
+	"github.com/test-network-function/test-network-function/pkg/tnf/handlers/generic"
+	"github.com/test-network-function/test-network-function/pkg/tnf/interactive"
+	"github.com/test-network-function/test-network-function/pkg/tnf/reel"
+)
+
+const (
+	// ocCmdMandatoryNumArgs is the number of positional arguments expected for "jsontest run oc"
+	ocCmdMandatoryNumArgs = 4
+
+	// shellCmdMandatoryNumArgs is the number of positional arguments expected for "jsontest run shell"
+	shellCmdMandatoryNumArgs = 1
+
+	// sshCmdMandatoryNumArgs is the number of positional arguments expected for "jsontest run ssh"
+	sshCmdMandatoryNumArgs = 3
+
+	// ptyMandatoryNumArgs is the number of positional arguments expected for "jsontest run pty"
+	ptyMandatoryNumArgs = 2
+
+	// ptyMandatoryNumArgs is the number of positional arguments expected for "jsontest run pty-template"
+	ptyTemplateMandatoryNumArgs = 3
+
+	// testCreationErrorExitCode is the Unix return code used when a test fails to instantiate due to issues with the
+	// underlying Unix subprocess.
+	testCreationErrorExitCode = iota + 100
+
+	// testDidNotParseExitCode is the Unix return code used when an improperly formatted or JSON file is provided to the
+	// program.
+	testDidNotParseExitCode
+
+	// testDoesNotConformToSchemaExitCode is the Unix return code used when the JSON file provided does not adhere to the
+	// generic-test.schema.json JSON schema.
+	testDoesNotConformToSchemaExitCode
+
+	// testExpecterCreationFailedExitCode is the Unix return code when an expect.Expecter fails to properly instantiate.
+	testExpecterCreationFailedExitCode
+
+	// testMarshalErrorExitCode is the Unix return code used when the JSON file fails to Marshal correctly.  This may
+	// be caused by issues in the JSON payload.
+	testMarshalErrorExitCode
+
+	// testRunErrorExitCode is the Unix return code used when there is an error encountered while running the finite
+	// state machine.
+	testRunErrorExitCode
+
+	// inappropriateArguments is the Unix return code used when the supplied arguments are inappropriate for the given
+	// context.
+	inappropriateArguments
+
+	// ptyDoesNotConformToSchemaExitCode is the Unix return code used when the PTY JSON file provided does not adhere to
+	// the generic-pty.schema.json JSON schema.
+	ptyDoesNotConformToSchemaExitCode
+
+	// ptyMarshalErrorExitCode is the Unix return code used when the PTY JSON file fails to Marshal correctly.  This may
+	// be caused by issues in the JSON payload.
+	ptyMarshalErrorExitCode
+)
+
+var (
+	// rootCmd is the jsontest executable root.  Currently, the jsontest entrypoint has only one sub-command called
+	// "run", which runs a generic JSON test.
+	runCmd = &cobra.Command{
+		Use:   "jsontest",
+		Short: "A CLI for creating, validating, and running JSON test-network-function tests.",
+		Long:  `jsontest is a CLI library included in test-network-function used to prototype JSON test cases. The JSON test case can be run using oc, ssh, or local shell.`,
+	}
+
+	// runCmd is the json test executable option to run a JSON test.
+
+	// shellCmd is the entrypoint for running a test case on the local shell.
+	shellCmd = &cobra.Command{
+		Use:   "shell [jsonTestFile]",
+		Short: "run in a local shell context",
+		Args:  cobra.ExactValidArgs(shellCmdMandatoryNumArgs),
+		Run:   runShellCmd,
+	}
+
+	// sshCmd is the entrypoint for running a test case in an ssh shell.
+	sshCmd = &cobra.Command{
+		Use:   "ssh [user] [host] [jsonTestFile]",
+		Short: "run in an ssh context",
+		Args:  cobra.ExactValidArgs(sshCmdMandatoryNumArgs),
+		Run:   runSSHCmd,
+	}
+
+	// ocCmd is the entrypoint for running a test case in an interactive oc shell.
+	ocCmd = &cobra.Command{
+		Use:   "oc [namespace] [pod] [container] [jsonTestFile]",
+		Short: "run in an oc context",
+		Args:  cobra.ExactValidArgs(ocCmdMandatoryNumArgs),
+		Run:   runOcCmd,
+	}
+
+	// ptyCmd is the entrypoint for running a test case in a custom defined PTY.
+	ptyCmd = &cobra.Command{
+		Use:   "pty [ptyFile] [jsonTestFile]",
+		Short: "run a generic PTY command context",
+		Run:   runPTYCmd,
+	}
+
+	// ptyTemplateCmd is the entrypoint for running a test case in a custom defined, templated PTY.
+	ptyTemplateCmd = &cobra.Command{
+		Use:   "pty-template [ptyFile] [valuesFile] [jsonTestFile]",
+		Short: "run a generic templated PTY command context",
+		Run:   runPTYTemplateCmd,
+	}
+
+	// genericTestSchemaPath is the path to the generic-test.schema.json JSON schema relative to the program entrypoint.
+	genericTestSchemaPath = path.Join("schemas", generic.TestSchemaFileName)
+
+	// ptySchemaPath is the path to the generic-pty.schema.json JSON schema relative to the program entrypoint.
+	ptySchemaPath = path.Join("schemas", interactive.PTYSchemaFileName)
+)
+
+// fatalError reports a fatal error to stdout and exits.
+func fatalError(msg string, err error, exitCode int) {
+	log.Errorf("Fatal Error: %s caused by: %s", msg, err)
+	os.Exit(exitCode)
+}
+
+// createTest creates a tnf.Test using tester and handlers in the expecter context.
+func createTest(expecter *expect.Expecter, tester *tnf.Tester, handlers []reel.Handler, ch <-chan error) (*tnf.Test, error) {
+	return tnf.NewTest(expecter, *tester, handlers, ch)
+}
+
+// reportResults is a helper function used to log results to the console.
+func reportResults(tester *tnf.Tester, result int) {
+	log.Infof("Test Result: %d", result)
+	log.Info("Test Payload:")
+	testOutput, err := json.MarshalIndent(tester, "", "    ")
+	if err != nil {
+		fatalError("could not Marshal the test result", err, testMarshalErrorExitCode)
+	}
+	log.Info(string(testOutput))
+}
+
+// runTest creates and runs the tnf.Test implementation.
+func runTest(expecter *expect.Expecter, tester *tnf.Tester, handlers []reel.Handler, ch <-chan error) {
+	// Instantiate the test.
+	test, err := createTest(expecter, tester, handlers, ch)
+	if err != nil {
+		fatalError("could not create the test", err, testCreationErrorExitCode)
+	}
+
+	// Actually run the test in the expecter context.
+	result, err := test.Run()
+	if err != nil {
+		fatalError("could not run the test", err, testRunErrorExitCode)
+	}
+
+	// Human readable results output to the console.
+	reportResults(tester, result)
+}
+
+// setupAndRunTest is a helper function to run a JSON test in a given expecter context.
+func setupTest(file string) (*tnf.Tester, []reel.Handler) {
+	// Instantiate the test from JSON, ensuring that JSON validation succeeds.
+	tester, handlers, result, err := generic.NewGenericFromJSONFile(file, genericTestSchemaPath)
+	if err != nil {
+		fatalError("the supplied JSON test could not be parsed correctly", err, testDidNotParseExitCode)
+	}
+
+	// If the given JSON file does not parse against the generic test schema, report all of the problems in a human
+	// readable format.
+	if !result.Valid() {
+		log.Error("The supplied JSON does not conform to the generic-test.schema.json JSON schema.  Here are the problems we found:")
+		for _, e := range result.Errors() {
+			log.Errorf("- %v\n", e)
+		}
+		fatalError("the supplied JSON does not conform to the expected JSON schema",
+			fmt.Errorf("schema validation error"), testDoesNotConformToSchemaExitCode)
+	}
+	return tester, handlers
+}
+
+func runSSHCmd(_ *cobra.Command, args []string) {
+	// Extra careful check since cobra checks this for us.
+	if len(args) < sshCmdMandatoryNumArgs {
+		errString := fmt.Sprintf("oc requires %d arguments", sshCmdMandatoryNumArgs)
+		fatalError(errString, errors.New(errString), inappropriateArguments)
+	}
+
+	// Report what is being run to the user.
+	user, host, file := args[0], args[1], args[2]
+	log.Info(term.Bluef("Running %s from ssh interactive shell [user=\"%s\" host=\"%s\"]", file, user, host))
+
+	// setup / parse the input test.  Must be done prior to creating the Expecter to derive the test timeout.
+	tester, handlers := setupTest(file)
+
+	// SSH shell creation.
+	goExpectSpawner := interactive.NewGoExpectSpawner()
+	var spawnContext interactive.Spawner = goExpectSpawner
+	context, err := interactive.SpawnSSH(&spawnContext, user, host, (*tester).Timeout(), interactive.Verbose(true), interactive.SendTimeout((*tester).Timeout()))
+	if err != nil {
+		fatalError("could not create the ssh expecter", err, testExpecterCreationFailedExitCode)
+	}
+
+	// actually run the test.
+	runTest(context.GetExpecter(), tester, handlers, context.GetErrorChannel())
+}
+
+// runOcCmd is a helper function used to run JSON tests in an oc context.
+func runOcCmd(_ *cobra.Command, args []string) {
+	// Extra careful check since cobra checks this for us.
+	if len(args) < ocCmdMandatoryNumArgs {
+		errString := fmt.Sprintf("oc requires %d arguments", ocCmdMandatoryNumArgs)
+		fatalError(errString, errors.New(errString), inappropriateArguments)
+	}
+
+	// Report what is being run to the user.
+	namespace, pod, container, file := args[0], args[1], args[2], args[3]
+	log.Info(term.Bluef("Running %s from oc interactive shell [namespace=\"%s\" pod=\"%s\" container=\"%s\"]", file, namespace, pod, container))
+
+	// setup / parse the input test.  Must be done prior to creating the Expecter to derive the test timeout.
+	tester, handlers := setupTest(file)
+
+	// oc shell creation.
+	goExpectSpawner := interactive.NewGoExpectSpawner()
+	var spawnContext interactive.Spawner = goExpectSpawner
+	oc, ch, err := interactive.SpawnOc(&spawnContext, pod, container, namespace, (*tester).Timeout(), interactive.Verbose(true), interactive.SendTimeout((*tester).Timeout()))
+	if err != nil {
+		fatalError("could not create the oc expecter", err, testExpecterCreationFailedExitCode)
+	}
+
+	// actually run the test.
+	runTest(oc.GetExpecter(), tester, handlers, ch)
+}
+
+// runShellCmd is a helper function used to run JSON tests in a shell context.
+func runShellCmd(_ *cobra.Command, args []string) {
+	// Extra careful check since cobra checks this for us.
+	if len(args) < shellCmdMandatoryNumArgs {
+		errString := fmt.Sprintf("shell requires %d arguments", shellCmdMandatoryNumArgs)
+		fatalError(errString, errors.New(errString), inappropriateArguments)
+	}
+
+	// Report what is being run to the user.
+	file := args[0]
+	log.Info(term.Bluef("Running %s from a local shell context", file))
+
+	// setup / parse the input test.  Must be done prior to creating the Expecter to derive the test timeout.
+	tester, handlers := setupTest(file)
+
+	// Shell creation.
+	goExpectSpawner := interactive.NewGoExpectSpawner()
+	var spawnContext interactive.Spawner = goExpectSpawner
+	context, err := interactive.SpawnShell(&spawnContext, (*tester).Timeout(), interactive.Verbose(true))
+	if err != nil {
+		fatalError("could not create the shell expecter", err, testExpecterCreationFailedExitCode)
+	}
+
+	// actually run the test.
+	runTest(context.GetExpecter(), tester, handlers, context.GetErrorChannel())
+}
+
+// runPTYCmd runs a generic, off the shell, command using a pseudo-terminal.  This is particularly useful for quickly
+// testing and incorporating custom PTYs, such as a router CLI.
+func runPTYCmd(_ *cobra.Command, args []string) {
+	numArgs := len(args)
+	if numArgs < ptyMandatoryNumArgs {
+		errString := fmt.Sprintf("command requires %d arguments", ptyMandatoryNumArgs)
+		fatalError(errString, errors.New(errString), inappropriateArguments)
+	}
+
+	ptyFile, jsonTestFile := args[0], args[1]
+
+	goExpectSpawner := interactive.NewGoExpectSpawner()
+	var spawnContext interactive.Spawner = goExpectSpawner
+
+	context, result, err := interactive.SpawnGenericPTYFromYAMLFile(ptyFile, ptySchemaPath, &spawnContext)
+	tester, handlers := setupTest(jsonTestFile)
+
+	if err != nil {
+		fatalError("Couldn't create the PTY", err, ptyMarshalErrorExitCode)
+	}
+	if !result.Valid() {
+		log.Error("The supplied JSON does not conform to the generic-ptyFile.schema.json JSON schema.  Here are the problems we found:")
+		for _, e := range result.Errors() {
+			log.Errorf("- %v\n", e)
+		}
+		fatalError("the supplied JSON does not conform to the expected JSON schema",
+			fmt.Errorf("schema validation error"), ptyDoesNotConformToSchemaExitCode)
+	}
+
+	runTest(context.GetExpecter(), tester, handlers, context.GetErrorChannel())
+}
+
+// runPTYTemplateCmd runs a generic, off the shell, Go-templated command using a pseudo-terminal.  This is particularly
+// useful for quickly testing and incorporating custom PTYs, such as a router CLI, especially in cases in which the
+// arguments vary given the context (i.e., ftp <host>).
+func runPTYTemplateCmd(_ *cobra.Command, args []string) {
+	numArgs := len(args)
+	if numArgs < ptyTemplateMandatoryNumArgs {
+		errString := fmt.Sprintf("command requires %d arguments", ptyMandatoryNumArgs)
+		fatalError(errString, errors.New(errString), inappropriateArguments)
+	}
+
+	ptyTemplate, valuesFile, jsonTestFile := args[0], args[1], args[2]
+
+	goExpectSpawner := interactive.NewGoExpectSpawner()
+	var spawnContext interactive.Spawner = goExpectSpawner
+
+	context, result, err := interactive.SpawnGenericPTYFromYAMLTemplate(ptyTemplate, valuesFile, ptySchemaPath, &spawnContext)
+	tester, handlers := setupTest(jsonTestFile)
+
+	if err != nil {
+		fatalError("Couldn't create the PTY", err, ptyMarshalErrorExitCode)
+	}
+	if !result.Valid() {
+		log.Error("The rendered JSON does not conform to the generic-ptyTemplate.schema.json JSON schema.  Here are the problems we found:")
+		for _, e := range result.Errors() {
+			log.Errorf("- %v\n", e)
+		}
+		fatalError("the supplied JSON does not conform to the expected JSON schema",
+			fmt.Errorf("schema validation error"), ptyDoesNotConformToSchemaExitCode)
+	}
+
+	runTest(context.GetExpecter(), tester, handlers, context.GetErrorChannel())
+}
+
+// Execute executes the jsontest program, returning any applicable errors.
+func NewCommand() *cobra.Command {
+	runCmd.AddCommand(ocCmd, sshCmd, shellCmd, ptyCmd, ptyTemplateCmd)
+	return runCmd
+}
