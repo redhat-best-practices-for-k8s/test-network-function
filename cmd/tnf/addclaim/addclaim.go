73ac52c1b5449db3f5f546808f117081acdc50bf
aabughosh <aabugosh@redhat.com>
2021-11-02 15:29:05 +0200

    change package name

--- cmd/tnf/addclaim/addclaim.go
+++ cmd/tnf/addclaim/addclaim.go
@@ -1,4 +1,4 @@
-package addclaim
+package claim
 
 import (
 	"fmt"
--- cmd/tnf/main.go
+++ cmd/tnf/main.go
@@ -4,7 +4,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	"github.com/test-network-function/test-network-function/cmd/tnf/addclaim"
+	claim "github.com/test-network-function/test-network-function/cmd/tnf/addclaim"
 	"github.com/test-network-function/test-network-function/cmd/tnf/generate/catalog"
 	"github.com/test-network-function/test-network-function/cmd/tnf/generate/handler"
 	"github.com/test-network-function/test-network-function/cmd/tnf/grade"
@@ -24,7 +24,7 @@ var (
 )
 
 func main() {
-	rootCmd.AddCommand(addclaim.NewCommand())
+	rootCmd.AddCommand(claim.NewCommand())
 	rootCmd.AddCommand(generate)
 	generate.AddCommand(catalog.NewCommand())
 	generate.AddCommand(handler.NewCommand())
