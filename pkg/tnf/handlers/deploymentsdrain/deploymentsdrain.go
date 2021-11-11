5b323a9339d9b782ebb27ec0bfbd5263d9ab238f
David Elie-Dit-Cosaque <deliedit@redhat.com>
2021-11-11 12:36:33 -0600

    drain  command failure not properly exiting

--- pkg/tnf/handlers/deploymentsdrain/deploymentsdrain.go
+++ pkg/tnf/handlers/deploymentsdrain/deploymentsdrain.go
@@ -83,8 +83,10 @@ func (dd *DeploymentsDrain) ReelFirst() *reel.Step {
 }
 
 // ReelMatch sets result
-func (dd *DeploymentsDrain) ReelMatch(_, _, _ string) *reel.Step {
-	dd.result = tnf.SUCCESS
+func (dd *DeploymentsDrain) ReelMatch(_, _, match string) *reel.Step {
+	if match != "" {
+		dd.result = tnf.SUCCESS
+	}
 	return nil
 }
 
