9799353afae5e22f7663a59638f46b4bd9919bc8
David Elie-Dit-Cosaque <deliedit@redhat.com>
2021-12-03 08:01:53 -0600

    PR review from Jun and Gonzalo

--- test-network-function/certification/suite.go
+++ test-network-function/certification/suite.go
@@ -59,33 +59,33 @@ func testContainerCertificationStatus() {
 	testID := identifiers.XformToGinkgoItIdentifier(identifiers.TestContainerIsCertifiedIdentifier)
 	ginkgo.It(testID, func() {
 		env := configpkg.GetTestEnvironment()
-		cnfsToQuery := env.Config.CertifiedContainerInfo
+		containersToQuery := env.Config.CertifiedContainerInfo
 
-		if len(cnfsToQuery) == 0 {
+		if len(containersToQuery) == 0 {
 			ginkgo.Skip("No containers to check configured in tnf_config.yml")
 		}
 
-		ginkgo.By(fmt.Sprintf("Getting certification status. Number of containers to check: %d", len(cnfsToQuery)))
+		ginkgo.By(fmt.Sprintf("Getting certification status. Number of containers to check: %d", len(containersToQuery)))
 
-		if len(cnfsToQuery) > 0 {
+		if len(containersToQuery) > 0 {
 			certAPIClient = api.NewHTTPClient()
-			allCnfToQueryEmpty := true
-			for _, cnf := range cnfsToQuery {
-				if cnf.Name == "" || cnf.Repository == "" {
-					tnf.ClaimFilePrintf(" cnf name = \"%s\" or repository = \"%s\" is missing, skipping this cnf to query", cnf.Name, cnf.Repository)
+			allContainersToQueryEmpty := true
+			for _, c := range containersToQuery {
+				if c.Name == "" || c.Repository == "" {
+					tnf.ClaimFilePrintf("Container name = \"%s\" or repository = \"%s\" is missing, skipping this container to query", c.Name, c.Repository)
 					continue
 				}
-				allCnfToQueryEmpty = false
-				cnf := cnf // pin
+				allContainersToQueryEmpty = false
+				c := c // pin
 				// Care: this test takes some time to run, failures at later points while before this has finished may be reported as a failure here. Read the failure reason carefully.
-				ginkgo.By(fmt.Sprintf("container %s/%s should eventually be verified as certified", cnf.Repository, cnf.Name))
+				ginkgo.By(fmt.Sprintf("Container %s/%s should eventually be verified as certified", c.Repository, c.Name))
 				gomega.Eventually(func() bool {
-					isCertified := certAPIClient.IsContainerCertified(cnf.Repository, cnf.Name)
+					isCertified := certAPIClient.IsContainerCertified(c.Repository, c.Name)
 					return isCertified
 				}, eventuallyTimeoutSeconds, interval).Should(gomega.BeTrue())
 			}
-			if allCnfToQueryEmpty {
-				ginkgo.Skip("No containers to check because either cnf name or repository is empty for all cnfs in tnf_config.yml")
+			if allContainersToQueryEmpty {
+				ginkgo.Skip("No containers to check because either container name or repository is empty for all containers in tnf_config.yml")
 			}
 		}
 	})
@@ -106,11 +106,11 @@ func testOperatorCertificationStatus() {
 			allOperatorsToQueryEmpty := true
 			for _, certified := range operatorsToQuery {
 				if certified.Name == "" || certified.Organization == "" {
-					tnf.ClaimFilePrintf(" operator name = \"%s\" or organization = \"%s\" is missing, skipping this cnf to query", certified.Name, certified.Organization)
+					tnf.ClaimFilePrintf("Operator name = \"%s\" or organization = \"%s\" is missing, skipping this operator to query", certified.Name, certified.Organization)
 					continue
 				}
 				allOperatorsToQueryEmpty = false
-				ginkgo.By(fmt.Sprintf("should eventually be verified as certified (operator %s/%s)", certified.Organization, certified.Name))
+				ginkgo.By(fmt.Sprintf("Should eventually be verified as certified (operator %s/%s)", certified.Organization, certified.Name))
 				// Care: this test takes some time to run, failures at later points while before this has finished may be reported as a failure here. Read the failure reason carefully.
 				certified := certified // pin
 				gomega.Eventually(func() bool {
