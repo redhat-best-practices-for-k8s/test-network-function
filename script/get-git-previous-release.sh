c142a84a723ccfa170b0bb773bf1b1667eccc634
Javier Pena <jpena@redhat.com>
2022-01-13 10:14:12 +0100

    Improve git management in Makefile and release workflow
    Previously, we were using some custom shell scripts to get the current
    and previous tag in the Makefile and release actions. This commit
    changes that to plain git commands, to improve readability and remove
    some workarounds (like checking out files from main on a branch).

--- script/get-git-previous-release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#set -x
-
-GIT_PREVIOUS_RELEASE=$(comm -12 all-releases.txt latest-release-tag.txt)
-echo $GIT_PREVIOUS_RELEASE
--- script/get-git-release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#set -x
-
-GIT_RELEASE=$(comm -12 all-releases.txt release-tag.txt)
-echo $GIT_RELEASE 
